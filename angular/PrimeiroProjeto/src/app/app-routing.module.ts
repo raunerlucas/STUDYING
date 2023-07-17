472c98966af3a72eb432325908fc69a394b8d303
Rauner Lucas <raunerlucas@gmail.com>
2023-07-17 15:06:54 -0300

    aula17

--- angular/PrimeiroProjeto/src/app/app-routing.module.ts
+++ angular/PrimeiroProjeto/src/app/app-routing.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: FirstComponentComponent},
+  { path: 'list',component: ListRenderComponent},
+  { path: 'parentData',component: ParentDataComponent},
+  { path: 'directives',component: DirectivesComponent},
+  { path: 'ifRender',component: IfRenderComponent},
+  { path: 'eventos',component: EventosComponent},
+  { path: 'Emitter',component: EmitterComponent},
+  { path: 'pipes',component: PipesComponent},
+  { path: 'twobinding',component: TwoWayBindingComponent}
+
+]
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- angular/PrimeiroProjeto/src/app/app.module.ts
+++ angular/PrimeiroProjeto/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
