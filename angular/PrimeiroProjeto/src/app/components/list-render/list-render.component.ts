42f1a3e4b8790e14a7cb59e31da73c9f7f286208
Rauner Lucas <raunerlucas@gmail.com>
2023-07-17 14:27:01 -0300

    aula16

--- angular/PrimeiroProjeto/src/app/components/list-render/list-render.component.ts
+++ angular/PrimeiroProjeto/src/app/components/list-render/list-render.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
+
 export class ListRenderComponent {
   animals: Animal[] = [
     {name: "Turca", type: "Dog", age: 3},
@@ -21,4 +24,11 @@ export class ListRenderComponent {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`
   }
 
+  constructor(private listService: ListService){
+  
+  }
+
+  removeAnimal(animal: Animal){
+    this.animals = this.listService.remove(this.animals, animal);
+  }
 }
