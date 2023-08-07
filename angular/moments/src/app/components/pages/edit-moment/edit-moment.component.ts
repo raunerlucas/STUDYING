ce740c0070bd661c60b50dd972bd708250e6c76d
Rauner Lucas <raunerlucas@gmail.com>
2023-08-07 16:51:07 -0300

    aula39

--- angular/moments/src/app/components/pages/edit-moment/edit-moment.component.ts
+++ angular/moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -5,6 +5,8 @@ import { Moment } from 'src/app/Moments';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
 import { environment } from 'src/environments/environment';
+import { MessagesService } from 'src/app/services/messages.service';
+
 
 @Component({
   selector: 'app-edit-moment',
@@ -20,6 +22,7 @@ export class EditMomentComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private momentService: MomentService,
+    private messagesService: MessagesService
     ){}
 
 
@@ -31,5 +34,23 @@ export class EditMomentComponent implements OnInit {
     )
   }
 
+  async editHandler(momentData: Moment){
+    const id = this.moment.id
+    
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+
+    if (momentData.image){
+      formData.append('image', momentData.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messagesService.add(`Momento ${id} foi editado com sucesso!`)
+
+    this.router.navigate(['/']);
+  }
 
 }
