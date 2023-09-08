cab549c000743c1e9a9877c6b21e63b26c995ae9
Rauner Lucas <raunerlucas@gmail.com>
2023-09-08 20:10:37 -0300

    Studying Modulos

---
+++ AngularDIO/Modulo-Proj/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/*
+
+  Aqui na shared é colocado os modulos que vão ser compartilhados entre os outros componentes
+
+*/
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
