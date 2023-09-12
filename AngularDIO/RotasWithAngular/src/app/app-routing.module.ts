861fd7c7a911a609be9935d657f10792e2c41f7c
Rauner Lucas <raunerlucas@gmail.com>
2023-09-12 15:38:58 -0300

    wording with the rotas

--- AngularDIO/RotasWithAngular/src/app/app-routing.module.ts
+++ AngularDIO/RotasWithAngular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: TitleComponent, pathMatch: 'full'},
+  {path: 'portifolio', component: CardComponent},
+  // Rota coriga no caso essa vai redirecionar para o TitleComponent
+  {path: '**', redirectTo: ''}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
