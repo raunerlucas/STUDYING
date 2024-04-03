2fad969c3f167433ad263b510b275e25ad871ff2
raunerlucas <raunerlucas@gmail.com>
2024-04-03 17:28:14 -0300

    Stuying Nexts

--- JavaFuncManoelCampos/Exep2/src/main/java/exercicios/aulas/Aula7.java
+++ JavaFuncManoelCampos/Exep2/src/main/java/exercicios/aulas/Aula7.java
@@ -1,2 +1,54 @@
-package exercicios.aulas;public class Aula7 {
+package exercicios.aulas;
+
+import exercicios.base.Aula;
+import exercicios.model.Estudante;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
+
+public class Aula7 extends Aula {
+
+    public Aula7() {
+ /*       OptionalDouble maxNotaestudantesOpitiona = estudantes.stream()
+                .filter(e -> e.getSexo() == 'M' )
+                .mapToDouble(Estudante::getNota)
+                .max();
+//        double maiorNota = maxNotaestudantesOpitiona.orElse(0);
+//        double maiorNota = maxNotaestudantesOpitiona.orElseGet(Aula7::getNotaAleatoria);
+        double maiorNota = maxNotaestudantesOpitiona.orElseThrow(
+                () -> new NoSuchElementException("Não foi encontrado nenhum Estudante")
+        );
+
+        System.out.println(maiorNota);*/
+
+        estudantes.stream()
+                .filter(e -> e.getSexo() == 'M' )
+                .mapToDouble(Estudante::getNota)
+                .max()
+                .ifPresentOrElse(System.out::println,
+                        () -> System.out.println("Não foi encontrado nenhum Estudante"));
+    }
+
+    private void imprimenota(@Nullable Estudante estudante) {
+        if (estudante == null) {
+            System.out.println("Estudante não encontrado");
+            return;
+        }
+        System.out.println(estudante.getNota());
+    }
+
+    public static void main(String[] args) {
+        new Aula7().start();
+    }
+
+    private static double getNotaAleatoria() {
+        var x = Math.random();
+        var z = Math.random();
+        return x + z;
+    }
+
+    @Override
+    public void start() {
+    }
 }
---
+++ Next/Exep1/dapp-nft/src/pages/mint.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Mint Pag</title>
+    </Head>
+    <main>
+        <h1>Mint Page</h1>
+    </main>
+    </>
+  );
+}
